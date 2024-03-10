5884d4ceae3f10946b525875ecb9a73c64772c20
Ubaida19 <141411875+Ubaida19@users.noreply.github.com>
2024-03-10 12:26:23 +0500

    intersection of arrays

---
+++ array_intersection.py
@@ -0,0 +1,17 @@
+from typing import List
+def intersection(nums1:List[int],nums2:List[int])->List[int]:
+    nums1 = set(nums1)
+    nums2 = set(nums2)
+    return list(nums1 & nums2)
+    
+    
+def main():
+    nums1 = [1,2,2,1]
+    nums2 = [2,2]
+    print(intersection(nums1,nums2))
+    nums3 = [4,9,5]
+    nums4 = [9,4,9,8,4]
+    print(intersection(nums3,nums4))
+
+if __name__ == '__main__':
+    main()
